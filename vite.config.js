8f41f4344b033067f9042d3b535a0db1f9d0e4bd
autostralis <autostralisofficial@gmail.com>
2025-08-17 00:10:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 
+// Use '/' on Vercel, '/paarvathy-ammal/' on GitHub Pages
+const isVercel = process.env.VERCEL; // Vercel sets this env var
+const base = isVercel ? '/' : '/paarvathy-ammal/';
+
 export default defineConfig({
-  base: '/paarvathy-ammal/',
+  base,
   plugins: [react()],
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
